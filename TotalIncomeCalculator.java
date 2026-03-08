917ad53b6b35f7190c1fc878f8fa4d41a376ae3a
ac1442Aditya <ac1442@srmist.edu.in>
2026-03-08 23:58:52 +0530

    Create TotalIncomeCalculator.java

---
+++ TotalIncomeCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TotalIncomeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the salary in INR: ");
+        double salary = sc.nextDouble();
+
+        System.out.print("Enter the bonus in INR: ");
+        double bonus = sc.nextDouble();
+
+        double totalIncome = salary + bonus;
+
+        System.out.println("The salary is INR " + salary + " and bonus is INR " + bonus +
+                           ". Hence Total Income is INR " + totalIncome);
+
+        sc.close();
+    }
+}
