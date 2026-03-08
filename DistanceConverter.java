52f5cd2155d7a8eb265703ca39b4482edceaa8f2
ac1442Aditya <ac1442@srmist.edu.in>
2026-03-08 23:48:56 +0530

    Create DistanceConverter.java

---
+++ DistanceConverter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DistanceConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the distance in feet: ");
+        double distanceInFeet = sc.nextDouble();
+
+        // Convert feet to yards: 1 yard = 3 feet
+        double distanceInYards = distanceInFeet / 3;
+
+        // Convert yards to miles: 1 mile = 1760 yards
+        double distanceInMiles = distanceInYards / 1760;
+
+        System.out.println("The distance in yards is " + distanceInYards +
+                           " while the distance in miles is " + distanceInMiles);
+
+        sc.close();
+    }
+}
